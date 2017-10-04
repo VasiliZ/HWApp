4510466f8fcdc0398ec47cd7ea31782f4b07ae0d
DESKTOP-4SMLB1E\Home <zaicev.vasili@gmail.com>
2017-10-04 14:02:52 +0300

    Add method onDestroy

--- app/src/main/java/com/github/vasiliz/hwapp/MainActivity.java
+++ app/src/main/java/com/github/vasiliz/hwapp/MainActivity.java
@@ -17,4 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
